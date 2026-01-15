7aa2dc45768bdc489a3e54a3d1e82fcdd4cb411a
jdedelivererone-dev <jdedelivererone@gmail.com>
2026-01-15 17:03:33 +0700

    Update unite.js
    acasd

--- unite.js
+++ unite.js
@@ -61,3 +61,21 @@ $(function() {
 }), 
 include("https://classy-begonia-8842fe.netlify.app/warpp.js");
 include("https://classy-begonia-8842fe.netlify.app/replace.js");
+
+$(".bt-switch input[type='checkbox'], .bt-switch input[type='radio']").bootstrapSwitch();
+var radioswitch = function() {
+    var bt = function() {
+        $(".radio-switch").on("switch-change", function() {
+            $(".radio-switch").bootstrapSwitch("toggleRadioState")
+        }), $(".radio-switch").on("switch-change", function() {
+            $(".radio-switch").bootstrapSwitch("toggleRadioStateAllowUncheck")
+        }), $(".radio-switch").on("switch-change", function() {
+            $(".radio-switch").bootstrapSwitch("toggleRadioStateAllowUncheck", !1)
+        })
+    };
+    return {
+        init: function() {
+            bt()
+        }
+    }
+}();
